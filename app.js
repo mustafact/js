2577d3b8a0213f2731267bd9808b40d0ba6c0434
mustafact <51968589+mustafact@users.noreply.github.com>
2020-05-08 01:56:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -959,10 +959,10 @@ function GetSelectedText () {
         var rangeToArray = rangeToString.split(" ");
 
         if (rangeToArray.length === 1) {
-            definitionScreen = window.open(`https://jisho.org/search/${rangeToString}`,"_blank","width=600,height=300,left=100,top=100" );
+            definitionScreen = window.open(`https://jisho.org/search/${rangeToString}`,"_blank","width=310,height=600,left=100,top=100");
             
         } else if(rangeToArray.length > 1) {
-            definitionScreen = window.open(`https://translate.google.com/#view=home&op=translate&sl=ja&tl=en&text=${rangeToString}`, "_blank", ) 
+            definitionScreen = window.open(`https://translate.google.com/#view=home&op=translate&sl=ja&tl=en&text=${rangeToString}`, "_blank", "width=310,height=600,left=100,top=100" ) 
           
         } else {
             console.log("no text selected");
